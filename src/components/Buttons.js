8e5c7d74cd32ee88af266c1b9d86f8fbc1adaa5b
LunaLiu4 <luna.liu829@gmail.com>
2023-07-15 01:18:34 -0500

    grade assignment6

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,24 @@
-const Buttons = () => {
+import satData from "./satData";
+import style from './styling.css';
+import React from 'react';
+
+const Buttons = (props) => {
+  const displaySats = props.displaySats;
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })
+      }
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div >
   );
 };
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,29 @@
-const Table = ({ sat }) => {
+import satData from './satData';
+import style from './styling.css';
+import React from 'react';
+
+const Table = (props) => {
+
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+      </thead>
+      <tbody>
+        {props.sat.map((data, id) =>
+          <tr key={id}>
+            <td >{data.name}</td>
+            <td >{data.type}</td>
+            <td >{data.launchDate}</td>
+            <td >{`${data.operational}`}</td>
+          </tr>)}
+      </tbody>
+    </table>
   );
 };
 
